e1bb2bff4d2d2310eaad757de00bdc2cb0320ade
thegreatbullet <clementlum@rocketmail.com>
2022-06-06 19:17:06 +0800

    initial initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+import React, { useState, useEffect } from 'react'
 
-function App() {
+import Search from './components/Search/Search'
+import Card from './components/Card/Card'
+import Pagination from './components/Pagination/Pagination'
+import Filter from './components/Filter/Filter'
+import Navbar from './components/Navbar/Navbar'
+
+export default function App() {
+  let api = `https://rickandmortyapi.com/api/character/?page=2`
+  let [fetchedData, updateFetchedData] = useState([])
+  let { info, results } = fetchedData
+
+  useEffect(() => {
+    ;(async function () {
+      let data = await fetch(api).then((res) => res.json())
+      updateFetchedData(data)
+    })()
+  }, [api])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='text-center mb-3'>Characters</h1>
+      <div className='container'>
+        <div className='row'>
+          Filter component
+          <div className='col-lg-8 col-12'>
+            <div className='row'>
+              <Card results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
